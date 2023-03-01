c8036a9ad5e06479c3eb5eaf07e20c6ba6f126c4
Bhavishm009 <bhavishm009@gmail.com>
2023-03-01 11:05:25 +0530

    New Update

--- src/utils/fetchFromApi.js
+++ src/utils/fetchFromApi.js
@@ -4,7 +4,7 @@ export const BASE_URL = 'https://youtube-v31.p.rapidapi.com';
 
 const options = {
   params: {
-    maxResults: 50,
+    maxResults: 10,
   },
   headers: {
     'X-RapidAPI-Key': 'KJwZZIJSFimshuivMSVGaiYzkRomp15f2vKjsnK4bKzuUzVLzA',
